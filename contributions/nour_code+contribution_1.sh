290ae89e15d16c9a5329104f57878e05ff393354
nourelislam <41902596+nourelislam@users.noreply.github.com>
2019-05-11 01:50:29 +0200

    Update nour_code+contribution_1.sh

--- contributions/nour_code+contribution_1.sh
+++ contributions/nour_code+contribution_1.sh
@@ -1,6 +1,8 @@
 The first pipeline 
 
-First of all, I worked on a dataset SRR8115017.fastq, I have decided to work with bowtie2 for alignment because I read their documentation I found their indexing is more efficient than BWA in addition to it’s manual is more readable and informative, so before alignment step I made me mind to read the whole manual  and understand almost their arguments.
+First of all, I worked on a dataset SRR8115017.fastq, I have decided to work with bowtie2 for alignment because I read their documentation and
+I found their indexing is more efficient than BWA in addition to it’s manual is more readable and informative,
+so before alignment step I made me mind to read the whole manual and understand almost their arguments.
 
 Downloading the ref.
 wget ftp://ftp.ensembl.org/pub/release-96/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.chromosome.21.fa.gz
@@ -8,7 +10,11 @@ wget ftp://ftp.ensembl.org/pub/release-96/fasta/homo_sapiens/dna/Homo_sapiens.GR
 Building ref. index
 bowtie2-build ~/BaseRecalibration_Benchmarking/Homo_sapiens.GRCh38.dna.chromosome.21.fa index_two_bowtie2/Homo_sapiens.fa
 
-Before beginning in the alignment step, I remember from the assignment that when I run the haplotypecaller, It has an error that due to the sample name and something like that, so I decided to include in the SAM file the header informations that includes the read group, platform unit, library preparation and sample name, I know that I am working with just one sample so it has the same library preparation in addition to other parameters but I feel that their absence is the cause of haplotypecaller error.
+Before beginning in the alignment step, I remembered from the assignment that when I run the haplotypecaller,
+It has an error that due to the sample name,read group and something like that, so I decided to include in the SAM file
+the header informations that includes the read group, platform unit, library preparation and sample name, 
+I know that I am working with just one sample so it has the same library preparation 
+in addition to other parameters but I feel that their absence is the cause of haplotypecaller error.
 
 R1="$HOME/BaseRecalibration_Benchmarking/SRR8115017.fastq.gz”
 RGID=$(cat $R1 | head -n1 | sed 's/:/_/g' |cut -d "." -f1)
@@ -25,9 +31,12 @@ bowtie2 -p 20 -q --no-unal -x index/Homo_sapiens.fa -U SRR8115017.fastq.gz --rg-
 However, after sorting by samtools and begin in the step of Markduplicate this error appeared
 Error in markduplicate:
 Exception in thread "main" htsjdk.samtools.SAMFormatException: Error parsing SAM header. Problem parsing @RG key:value pair. Line:
-@RG	ID:@SRR8115017	SM=SRR8115017	PL=Illumina	LB=SRR8115017_same	PU=@SRR8115017.; File /home/nourelislam/BaseRecalibration_Benchmarking/test_two.bam; Line number 3
+@RG	ID:@SRR8115017	SM=SRR8115017	PL=Illumina	LB=SRR8115017_same	PU=@SRR8115017.;
+File /home/nourelislam/BaseRecalibration_Benchmarking/test_two.bam; Line number 3
 
-So I realized that the error is due to the formatting of the SAM header, and after many trials, I found the error in the “ = ” sign, so I’ve changed it to “ : ” and repeat the alignment step and the Markduplicate step then the runnin is going smoothly.
+So I realized that the error is due to the formatting of the SAM header, and after many trials,
+I found the error in the “ = ” sign, so I’ve changed it to “ : ” and repeat the alignment step and the Markduplicate step
+then the running is going smoothly.
 
 bowtie2 -p 20 -q --no-unal -x index_two_bowtie2/Homo_sapiens.fa -U SRR8115017.fastq.gz --rg-id $RGID --rg SM:$SM --rg PL:$PL --rg LB:$LB --rg PU:$PU 2> align_stats.txt| samtools view -Sb -o bowtie2.bam
 
@@ -53,9 +62,12 @@ wget ftp://ftp.ensembl.org/pub/release-96/variation/vcf/homo_sapiens/homo_sapien
 Indexing:
 gatk IndexFeatureFile -F Homo_sapiens_chr21.vcf
 
-Error A USER ERROR has occurred: Error while trying to create index for /home/nourelislam/BaseRecalibration_Benchmarking/Homo_sapiens_chr21.vcf. Error was: htsjdk.tribble.TribbleException: The provided VCF file is malformed at approximately line number 1291601: empty alleles are not permitted in VCF records
+Error A USER ERROR has occurred: Error while trying to create index 
+for /home/nourelislam/BaseRecalibration_Benchmarking/Homo_sapiens_chr21.vcf. Error was: htsjdk.tribble.TribbleException: 
+The provided VCF file is malformed at approximately line number 1291601: empty alleles are not permitted in VCF records
 
-After searching about this error which is a normal error in the vcf file, some suggested to completely remove the record having that malformation, so when I run the following code and remove the specific malformed record, it runs smoothly.
+After searching about this error which is a normal error in the vcf file, some suggested to completely remove the record having that malformation,
+so when I run the following code and remove the specific malformed record, it runs smoothly.
 
 head -1291601 Homo_sapiens_chr21.vcf | tail -1  Then remove this record 
 
